67f5b27ed1d8e65760bdbf7b1131c6e9412e1b48
Symryvvin <symryvvin@gmail.com>
2021-03-03 14:02:36 +0300

    fix: fix error while password not set

--- identity/src/main/java/ru/aizen/account/management/domain/user/User.java
+++ identity/src/main/java/ru/aizen/account/management/domain/user/User.java
@@ -30,6 +30,12 @@ public class User {
 		this.phone = phone;
 	}
 
+	public User(String username, String password, Email email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
 	public static User register(String username, String email, String password) {
 		Assert.hasLength(username, "Username is required");
 		Assert.hasLength(email, "Email is required");
@@ -37,7 +43,7 @@ public class User {
 		if (username.length() > 256) {
 			throw new IllegalArgumentException("The username must be a string with a maximum length of " + MAX_USERNAME_LENGTH);
 		}
-		return new User(username, null, null, Email.from(email), null);
+		return new User(username, password, Email.from(email));
 	}
 
 	public void update(String firstName, String lastName, String email, String phone) {
