1b0a174f1ef4777ff08ca2bc828194e47c6944f0
Symryvvin <symryvvin@gmail.com>
2021-03-04 12:16:58 +0300

    feat: implement all routes in api gateway config

--- api-gateway/src/main/java/ru/aizen/apigateway/config/RouteConfig.java
+++ api-gateway/src/main/java/ru/aizen/apigateway/config/RouteConfig.java
@@ -8,25 +8,34 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RouteConfig {
 
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        String identityService = System.getenv("IDENTITY_SERVICE_URL");
-        System.out.println(identityService);
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		String identityService = System.getenv("IDENTITY_SERVICE_URL");
 
-        return builder.routes()
-                .route("registration", r -> r.path("/registration")
-                        .filters(f -> f.rewritePath("/registration", "/api/v1/user/"))
-                        .uri(identityService))
-                .route("my_profile",
-                        route -> route
-                                .path("/profile/me")
-                                .filters(f -> f.rewritePath("/profile/me", "/api/v1/user/1"))
-                                .uri(identityService))
-                .route("user_profile",
-                        route -> route
-                                .path("/profile/**")
-                                .filters(f -> f.rewritePath("/profile/(?<segment>.*)", "/api/v1/user/${segment}"))
-                                .uri(identityService))
-                .build();
-    }
+		return builder.routes()
+				.route("registration",
+						route -> route.path("/registration")
+								.filters(f -> f.rewritePath("/registration", "/api/v1/user/registration"))
+								.uri(identityService))
+				.route("login",
+						route -> route.path("/login")
+								.filters(f -> f.rewritePath("/login", "/api/v1/user/login"))
+								.uri(identityService))
+				.route("my_profile",
+						route -> route
+								.path("/profile/me")
+								.filters(f -> f.rewritePath("/profile/me", "/api/v1/user/johndoe88"))
+								.uri(identityService))
+				.route("user_profile",
+						route -> route
+								.path("/profile/**")
+								.filters(f -> f.rewritePath("/profile/(?<segment>.*)", "/api/v1/user/${segment}"))
+								.uri(identityService))
+				.route("user_profile_edit",
+						route -> route
+								.path("/profile/**")
+								.filters(f -> f.rewritePath("/profile/(?<segment>.*)", "/api/v1/user/${segment}/edit"))
+								.uri(identityService))
+				.build();
+	}
 }
